1e858320b21a6f6639ad369023e961526c567395
vishnu300 <vishnukiran300@gmail.com>
2024-02-01 13:42:56 +0530

    intial

--- bin/prime_and_factorial_dart.dart
+++ bin/prime_and_factorial_dart.dart
@@ -1,13 +1,17 @@
+import 'dart:io';
+
 void main() {
-  int num = 5;
+   print ("enter a number");
+  int num=int.parse(stdin.readLineSync()!);
   List factors = [];
+ 
 
   for (int i = 1; i <= num; i++) {
     if (num % i == 0) {
       factors.add(i);
     }
   }
-  print(factors);
+ 
   if (factors.length > 2) {
     print("$num is not a prime number");
   } else {
@@ -22,43 +26,3 @@ void main() {
   print(factorial(num));
   }
 }
-
-// import 'dart:io';
-
-// // Function to check if a number is prime
-// bool isPrime(int number) {
-//   if (number < 2) {
-//     return false;
-//   }
-//   for (int i = 2; i <= number ~/ 2; i++) {
-//     if (number % i == 0) {
-//       return false;
-//     }
-//   }
-//   return true;
-// }
-
-// // Function to calculate the factorial of a number
-// BigInt factorial(int number) {
-//   if (number == 0 || number == 1) {
-//     return BigInt.one;
-//   }
-//   return BigInt.from(number) * factorial(number - 1);
-// }
-
-// void main() {
-//   // Input: Get a number from the user
-//   stdout.write("Enter a number: ");
-//   int userNumber = int.parse(stdin.readLineSync()!);
-
-//   // Check if the number is prime
-//   if (isPrime(userNumber)) {
-//     print("$userNumber is a prime number.");
-//   } else {
-//     print("$userNumber is not a prime number.");
-//   }
-
-//   // Calculate and print the factorial of the number
-//   BigInt result = factorial(userNumber);
-//   print("Factorial of $userNumber is: $result");
-// }
